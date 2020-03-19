071c988c4e41901692083c480f458744e72ca079
Vladimir-665 <62171393+Vladimir-665@users.noreply.github.com>
2020-03-19 15:29:21 +0300

    Update Arrays.java

--- src/Arrays.java
+++ src/Arrays.java
@@ -8,11 +8,11 @@ public class Arrays {
         int[] arr = {1, 1, 0, 1, 0, 1, 0, 0};
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] / 1 == 0) {
-                arr[i] = 1;                                       //arr[i] = arr[i] != 1 ? 1 : 0;
+                arr[i] = 1;                                       
             } else {
                 arr[i] = 0;
             }
-            System.out.print((arr[i])); //arr[i] = 1;
+            System.out.print((arr[i])); 
         }
     }
 
